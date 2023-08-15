14dd904f194462c6fa4e5492f5f41dd6e4b7e805
Yuanxin Wang <44472038+ywang65@users.noreply.github.com>
2023-08-15 12:34:18 -0500

    Update transition_cells_identification.R

--- R/transition_cells_identification.R
+++ R/transition_cells_identification.R
@@ -6,47 +6,11 @@ library(dplyr)
 library(Seurat)
 library(scLink)
 
-# main
 # We divided our methodology into 4 steps:
 # Step 1: Data pre-processing
 # Step 2: Selecting cells for Pearson’s correlation coefficients calculation
 # Step 3: Top most variable genes selection
 # Step 4: Transition index calculation
-
-n_neighbors<-300
-res<-list()
-# Since we only use cells and their nearest neighbors to calculate gene pair-
-# wise Pearson's correlation coefficients, to save computational cost, we do 
-# this calculation cluster by cluster.
-for(i in data$seurat_clusters %>% unique()){
-        print(i)
-        data_sub<-subset(data,seurat_clusters == i)
-        if(ncol(data_sub)<n_neighbors)
-                next
-        data_sub<-FindVariableFeatures(data_sub, selection.method = "vst", nfeatures = 2000)
-        data_sub<- ScaleData(data_sub,vars.to.regress="nCount_RNA")
-        data_sub<- RunPCA(data_sub, features = VariableFeatures(object = data_sub))
-        data_sub<- FindNeighbors(data_sub, dims = 1: 20,annoy.metric='cosine')
-        # calculating gene pair-wise Pearson's correlation coefficients using gene_pearson function
-        pearson<-gene_pearson(data_sub,n_neighbor=n_neighbors)
-        # fine tuning  (optional)
-        pearson<-abs(pearson)
-        res_1<-apply(pearson,1,function(x) ks.test(x,pearson[1,],alternative='greater')$statistic)
-        res_2<-apply(pearson,1,function(x) ks.test(x,pearson[1,],alternative='less')$statistic)
-        res_tmp<-find_ks_d(pearson[which.max(res_1),],pearson[which.max(res_2),])
-        tmp<-apply(pearson,1,function(x) sum(abs(x)>min(unlist(res_tmp)) & abs(x)<max(unlist(res_tmp)),na.rm=T)/sum(abs(x)>=0,na.rm=T))
-        data_sub<-AddMetaData(data_sub,data.frame('pearson'=tmp))
-        cell<-data_sub@meta.data %>% filter(pearson>quantile(data_sub$pearson,0.8,na.rm=T)) %>% rownames()
-        hvg<-sort(apply(as.matrix(GetAssayData(data_sub[,cell])),1,function(x) var(x)),decreasing=T)[1:100] %>% names
-        pearson<-gene_pearson(data_sub,n_neighbor=n_neighbors,highly_variable_gene=hvg)
-        # calculating transition index based on gene pair-wise Pearson's correlation coefficients
-        res_1<-apply(pearson,1,function(x) ks.test(x,pearson[1,],alternative='greater')$statistic)
-        res_2<-apply(pearson,1,function(x) ks.test(x,pearson[1,],alternative='less')$statistic)
-        res[[as.character(i)]]<-find_ks_d(pearson[which.max(res_1),],pearson[which.max(res_2),])
-}
-res<-Reduce(function(x,y) rbind(x,y), res)
-data<-AddMetaData(data,data.frame('transition_index'=tmp))
-saveRDS(res,'transition_index.rds')
 
 gene_pearson<-function(data,highly_variable_gene=NULL,n_neighbor=200){
         res<-NULL
@@ -91,3 +55,39 @@ find_ks_d<-function(x,y){ # x:control
         return(list('w_min'=w_min,'w_max'=w_max))
 }
 
+# main
+n_neighbors<-300
+res<-list()
+# Since we only use cells and their nearest neighbors to calculate gene pair-
+# wise Pearson's correlation coefficients, to save computational cost, we do 
+# this calculation cluster by cluster.
+for(i in data$seurat_clusters %>% unique()){
+        print(i)
+        data_sub<-subset(data,seurat_clusters == i)
+        if(ncol(data_sub)<n_neighbors)
+                next
+        data_sub<-FindVariableFeatures(data_sub, selection.method = "vst", nfeatures = 2000)
+        data_sub<- ScaleData(data_sub,vars.to.regress="nCount_RNA")
+        data_sub<- RunPCA(data_sub, features = VariableFeatures(object = data_sub))
+        data_sub<- FindNeighbors(data_sub, dims = 1: 20,annoy.metric='cosine')
+        # calculating gene pair-wise Pearson's correlation coefficients using gene_pearson function
+        pearson<-gene_pearson(data_sub,n_neighbor=n_neighbors)
+        # fine tuning  (optional)
+        pearson<-abs(pearson)
+        res_1<-apply(pearson,1,function(x) ks.test(x,pearson[1,],alternative='greater')$statistic)
+        res_2<-apply(pearson,1,function(x) ks.test(x,pearson[1,],alternative='less')$statistic)
+        res_tmp<-find_ks_d(pearson[which.max(res_1),],pearson[which.max(res_2),])
+        tmp<-apply(pearson,1,function(x) sum(abs(x)>min(unlist(res_tmp)) & abs(x)<max(unlist(res_tmp)),na.rm=T)/sum(abs(x)>=0,na.rm=T))
+        data_sub<-AddMetaData(data_sub,data.frame('pearson'=tmp))
+        cell<-data_sub@meta.data %>% filter(pearson>quantile(data_sub$pearson,0.8,na.rm=T)) %>% rownames()
+        hvg<-sort(apply(as.matrix(GetAssayData(data_sub[,cell])),1,function(x) var(x)),decreasing=T)[1:100] %>% names
+        pearson<-gene_pearson(data_sub,n_neighbor=n_neighbors,highly_variable_gene=hvg)
+        # calculating transition index based on gene pair-wise Pearson's correlation coefficients
+        res_1<-apply(pearson,1,function(x) ks.test(x,pearson[1,],alternative='greater')$statistic)
+        res_2<-apply(pearson,1,function(x) ks.test(x,pearson[1,],alternative='less')$statistic)
+        res_tmp<-find_ks_d(pearson[which.max(res_1),],pearson[which.max(res_2),])
+        res[[as.character(i)]]<-apply(pearson,1,function(x) sum(abs(x)>min(unlist(res_tmp)) & abs(x)<max(unlist(res_tmp)),na.rm=T)/sum(abs(x)>=0,na.rm=T))
+}
+res<-Reduce(function(x,y) rbind(x,y), res)
+data<-AddMetaData(data,data.frame('transition_index'=tmp))
+saveRDS(res,'transition_index.rds')
